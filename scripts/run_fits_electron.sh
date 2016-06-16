dc098e4d9b5bb764c24a68292fb915f9a449e87e
Devin Taylor <dntaylor@wisc.edu>
2016-06-16 08:38:35 -0500

    add run script

---
+++ scripts/run_fits_electron.sh
@@ -0,0 +1,6 @@
+python DevTools/TagAndProbe/python/fitter.py electron -mc tagAndProbe/electron/dy_nlo.root -mcLO tagAndProbe/electron/dy_lo.root -data tagAndProbe/electron/data.root
+python DevTools/TagAndProbe/python/dumpTagProbeTreeHTML.py --data fits.root -i electronFits -o fits/
+python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py electron CutBasedIDVeto "Cut Based ID Veto"
+python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py electron CutBasedIDLoose "Cut Based ID Loose"
+python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py electron CutBasedIDMedium "Cut Based ID Medium"
+python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py electron CutBasedIDTight "Cut Based ID Tight"
