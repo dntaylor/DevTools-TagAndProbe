70174e006578ab4ab2197987217a8183ea735c18
Devin Taylor <dntaylor@wisc.edu>
2016-10-03 14:27:45 +0200

    add back

--- recipe/recipe.sh
+++ recipe/recipe.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # CMSSW packages
-# disable for now
-#pushd $CMSSW_BASE/src
-#git cms-merge-topic fcouderc:tnp_egm_80X
-#popd
+pushd $CMSSW_BASE/src
+git cms-merge-topic fcouderc:tnp_egm_80X
+popd
