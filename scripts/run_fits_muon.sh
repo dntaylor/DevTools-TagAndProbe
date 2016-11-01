fe61509c51fdb592061bfdfab89d3a896ad2500d
Devin Taylor <dntaylor@wisc.edu>
2016-11-01 04:34:50 -0500

    updated plotting of fits for hpp Id

--- scripts/run_fits_muon.sh
+++ scripts/run_fits_muon.sh
@@ -1,5 +1,9 @@
 python DevTools/TagAndProbe/python/fitter.py muon -mc tagAndProbe/muon/dy_nlo.root -mcLO tagAndProbe/muon/dy_lo.root -data tagAndProbe/muon/data.root
 python DevTools/TagAndProbe/python/dumpTagProbeTreeHTML.py --data fits_muon.root -i muonFits -o fits_muon/
+python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py muon HppLooseID "Hpp Loose ID"
+python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py muon HppLooseIsoFromLooseID "Hpp Loose Iso from Loose ID"
+python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py muon HppMediumID "Hpp Medium ID"
+python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py muon HppMediumIsoFromMediumID "Hpp Medium Iso from Medium ID"
 python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py muon LooseID "Loose ID"
 python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py muon LooseIsoFromLooseID "Loose Iso from Loose ID"
 python DevTools/TagAndProbe/python/plotTagProbeEfficiencies.py muon MediumID "Medium ID"
