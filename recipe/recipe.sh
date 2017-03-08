106b4cd068e53abb9d41ac3b46d1541df190fd09
Devin Taylor <dntaylor@wisc.edu>
2017-03-08 17:30:22 -0600

    turn off again for standard workflow

--- recipe/recipe.sh
+++ recipe/recipe.sh
@@ -4,5 +4,6 @@
 pushd $CMSSW_BASE/src
 # currently brings in a lot of junk
 #git cms-merge-topic fcouderc:tnp_egm_80X_Moriond17_v1.0
-git cms-merge-topic dntaylor:tnp_moriond_8026p1
+# rebased on 8026p1
+#git cms-merge-topic dntaylor:tnp_moriond_8026p1
 popd
