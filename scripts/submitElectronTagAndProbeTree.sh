a5bba2abf30f48b6d9339e83fdac551df898d20f
Devin Taylor <dntaylor@wisc.edu>
2017-03-08 20:44:21 -0600

    fewer files per job

--- scripts/submitElectronTagAndProbeTree.sh
+++ scripts/submitElectronTagAndProbeTree.sh
@@ -5,5 +5,5 @@ if [ "$1" == "" ]; then
 else
     NAME="$1"
 fi
-submit_job.py crabSubmit --sampleList $CMSSW_BASE/src/DevTools/TagAndProbe/data/datasetList_TagAndProbe_MC.txt --filesPerJob 20 "$DATE"_DevTools_TagAndProbe_Electron_80X_"$NAME" DevTools/TagAndProbe/test/electronTagAndProbeTree_cfg.py isMC=1
+submit_job.py crabSubmit --sampleList $CMSSW_BASE/src/DevTools/TagAndProbe/data/datasetList_TagAndProbe_MC.txt --filesPerJob 10 "$DATE"_DevTools_TagAndProbe_Electron_80X_"$NAME" DevTools/TagAndProbe/test/electronTagAndProbeTree_cfg.py isMC=1
 submit_job.py crabSubmit --sampleList $CMSSW_BASE/src/DevTools/TagAndProbe/data/datasetList_TagAndProbe_Data_Electron.txt --lumisPerJob 600 --applyLumiMask "Collisions16" "$DATE"_DevTools_TagAndProbe_Electron_80X_"$NAME" DevTools/TagAndProbe/test/electronTagAndProbeTree_cfg.py isMC=0
