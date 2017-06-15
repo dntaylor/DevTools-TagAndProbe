aeb16fa3991b2ce0b57755db5eaf2ce5575f5544
Devin Taylor <dntaylor@wisc.edu>
2017-06-15 12:19:49 -0500

    new branch setup

--- recipe/recipe.sh
+++ recipe/recipe.sh
@@ -2,8 +2,12 @@
 
 # CMSSW packages
 pushd $CMSSW_BASE/src
+
+# TODO: update to 92X
+# https://twiki.cern.ch/twiki/bin/view/CMSPublic/ElectronTagAndProbe
 # currently brings in a lot of junk
 #git cms-merge-topic fcouderc:tnp_egm_80X_Moriond17_v1.0
 # rebased on 8026p1
 #git cms-merge-topic dntaylor:tnp_moriond_8026p1
+
 popd
